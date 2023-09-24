0fb0222f5a77a6df8f17bd80b4219c6d67504527
Frokesy <akindeleayanfeoluwa@gmail.com>
2023-09-24 19:20:11 +0100

    designed header section for template 1

--- components/templates/Frokesy.tsx
+++ components/templates/Frokesy.tsx
@@ -1,15 +1,35 @@
 import React from "react";
 import Layout from "./Layout";
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 const Frokesy = () => {
   return (
     <>
       <Layout>
-        <a href="/api/pdf" download="generated_pdf.pdf" className="absolute top-10 left-10">
-          Download PDF
-        </a>
-        <h1>Generated PDF</h1>
-        <p>This text will be in the PDF!</p>
+        <div className="header bg-[rgb(27,31,36)] pl-6 text-[#fff]">
+          <div className="flex justify-between pt-3">
+            <div className="w-[50%] max-w-[350px] pt-6">
+              <div className="border-[3px] border-b-[#ccc] w-[15%]"></div>
+              <h2 className="lg:text-[24px] text-[16px] font-bold pt-6">
+                I&apos;m Ayanfeoluwa Akindele, a Frontend Engineer.
+              </h2>
+
+              <div className="socials pt-6 flex space-x-3">
+                <FaGithub size={18} />
+                <FaTwitter color="#0e6e97" size={18} />
+                <FaLinkedin color="#0e6e97" size={18} />
+              </div>
+            </div>
+
+            <div className="lg:h-[200px] lg:w-[225px] h-[100%] w-[150px]">
+              <img
+                src="/assets/dp2.png"
+                alt="dp"
+                className="h-[100%] w-[100%]"
+              />
+            </div>
+          </div>
+        </div>
       </Layout>
     </>
   );
